d95b6b71b1b76776cc59a09f0e71c352369021d5
SoheilSalehian <soheil.salehian@gmail.com>
2018-02-10 16:47:08 -0500

    Update the media URL

--- instagram/endpoint.go
+++ instagram/endpoint.go
@@ -8,7 +8,8 @@
 
 package instagram
 
-const accountMediaURL string = "https://www.instagram.com/%s/media?max_id=%s"
+// const accountMediaURL string = "https://www.instagram.com/%s/media?max_id=%s"
+const accountMediaURL string = "https://www.instagram.com/%s/?__a=1&max_id=%s"
 const accountInfoURL string = "https://instagram.com/%s/?__a=1"
 const mediaInfoURL string = "https://instagram.com/p/%s/?__a=1"
 const locationURL string = "https://www.instagram.com/explore/locations/%s/?__a=1&max_id=%s"
